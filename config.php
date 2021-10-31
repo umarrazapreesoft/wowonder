9cc7e9c32178fb899d01a3adba66c574175e802c
Umar Raza <umarraza@users.github.com>
2021-10-31 20:37:11 +0500

    initial commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "vidcqjei_staging.vidcake";
+
+// Site URL
+$site_url = "http://localhost/personal/wowonder"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "67087c93-c982-445d-b405-af0fd4ed24ca"; // Your purchase code, don't give it to anyone. 
+?>
---
+++ themes/elavenn/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'eLavenn';
+
+$themeFolder = 'elavenn';
+
+$themeAuthor = 'Teja Sundeep';
+
+$themeAuthorUrl = 'mailto:tsreddykarri94@gmail.com';
+
+$themeVirsion = '1.2';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
